642eecb3ec7d89fe5cb85a9380fc7d33fff96ae5
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-02 14:27:35 +0300

    Создал папку lesson02 и скопировал туда исходные файлы

---
+++ study_JS/lesson02/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const money = 10;
+
+const income = 5000;
+
+let addExpenses = 200;
+
+let deposit = 1000;
+
+const mission = 'Выполнить первое ДЗ';
+
+const period = '01.04.2021';
+
+alert('Доброго времени суток, Александр!');
+
+console.log('Моё первое домашнее задание');
