42cc923d49648d1ad8f98fa3c61bd5732e1aab39
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-02 15:23:45 +0300

    Выполненные задания по уроку lesson02

--- study_JS/lesson02/script.js
+++ study_JS/lesson02/script.js
@@ -1,17 +1,28 @@
 'use strict';
 
-const money = 10;
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period,
+    budgetDay; 
 
-const income = 5000;
+money = 50000;
+console.log('money:', typeof money);
+income = 'Фриланс';
+console.log('income:', typeof income);
+addExpenses = 'Бензин, Интернет, Ресторан';
+console.log('Длина строки addExpenses:', addExpenses.length);
+deposit = true;
+console.log('deposit:', typeof deposit);
+mission = 2e5;
+period = 6;
+console.log('Период равен', period, 'месяцев и Цель заработать', mission, 'рублей');
 
-let addExpenses = 200;
+addExpenses = addExpenses.toLowerCase(); 
+addExpenses = addExpenses.split(', ');
+console.log('Результат: ', addExpenses);
 
-let deposit = 1000;
-
-const mission = 'Выполнить первое ДЗ';
-
-const period = '01.04.2021';
-
-alert('Доброго времени суток, Александр!');
-
-console.log('Моё первое домашнее задание');
+budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
