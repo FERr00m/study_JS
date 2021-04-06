b5aed0ca0c88c4a3eac812e9834ad23676674057
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-06 13:34:33 +0300

    feat(script.js) выполнить задание по ДЗ lesson04

--- study_JS/lesson02/script.js
+++ study_JS/lesson02/script.js
@@ -1,53 +1,87 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?'),
+let money = +prompt('Ваш месячный доход?', 50000),
     income = 'Фриланс',
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 2e5,
-    period = 6,
-    expenses1 = prompt('Введите обязательную статью расходов:'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    expenses2 = prompt('Введите ещё одну обязательную статью расходов:'),
-    amount2 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = money - amount1 - amount2,
-    budgetDay = Math.round(budgetMonth / 30);
+    period = 6;
+    
+
+function showTypeOf(data) {
+  console.log(data, typeof(data));
+}
 
 
-console.log('money: ', typeof money);
-console.log('income: ', typeof income);
-console.log('deposit: ', typeof deposit);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей');
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+
+function getExpensesMonth(value1, value2) {
+  let sum = value1 + value2;
+  return sum;
+}
+
+
+function getTargetMonth(value1, value2) {
+  if (value2 === 0) {
+    return ('Невозможно расчитать период для достижения цели, так как бюджет на месяц равен нулю');
+  }
+  let result = Math.round(value1 / value2);
+  return result;
+}
+
+
+function getAccumulatedMonth(income, value1, value2) {
+  let result = income - value1 - value2;
+  return result;
+}
+
+
+function getStatusIncome(data) {
+  if (data > 1200) {
+  return ('У вас высокий уровень дохода');
+} else if (600 <= data && data <= 1200) {
+  return ('У вас средний уровень дохода');
+} else if (0 <= data && data < 600) {
+  return ('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  return ('Что то пошло не так');
+}
+}
+
+
+let expenses1 = prompt('Введите обязательную статью расходов:', 'бензин'),
+    amount1 = +prompt('Во сколько это обойдется?', 3000),
+    expenses2 = prompt('Введите ещё одну обязательную статью расходов:', 'интернет'),
+    amount2 = +prompt('Во сколько это обойдется?', 600),
+    accumulatedMonth = getAccumulatedMonth(money, amount1, amount2),
+    budgetDay = Math.round(accumulatedMonth / 30);
 
 addExpenses = addExpenses.toLowerCase(); 
 addExpenses = addExpenses.split(', ');
-console.log('Результат: ', addExpenses);
 
-console.log('Бюджет на месяц', budgetMonth);
-
-if (budgetMonth === 0) {
-  console.log('Невозможно расчитать период для достижения цели, так как бюджет на месяц равен нулю');
-} else {
-  console.log('Цель будет достигнута за', Math.round(mission / budgetMonth), 'месяцев(-а)');
-}
-
+console.log('Расходы за месяц', getExpensesMonth(amount1, amount2));
+console.log('Возможные расходы: ', addExpenses);
+console.log('Cрок достижения цели в месяцах:', getTargetMonth(mission, accumulatedMonth));
 console.log('Бюджет на день', budgetDay);
+console.log('Бюджет на месяц', accumulatedMonth);
+console.log(getStatusIncome(budgetDay));
 
-// Вариант 1
-// if (budgetDay > 1200) {
-//   console.log('У вас высокий уровень дохода');
-// } else if (600 <= budgetDay && budgetDay <= 1200) {
-//   console.log('У вас средний уровень дохода');
-// } else if (0 <= budgetDay && budgetDay < 600) {
-//   console.log('К сожалению у вас уровень дохода ниже среднего');
-// } else {
-//   console.log('Что то пошло не так');
-// }
 
-// Вариант 2
-console.log((budgetDay > 1200) ? 'У вас высокий уровень дохода' :
-  (600 <= budgetDay && budgetDay <= 1200) ? 'У вас средний уровень дохода' :
-  (0 <= budgetDay && budgetDay < 600) ? 'К сожалению у вас уровень дохода ниже среднего' :
-  'Что то пошло не так');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
